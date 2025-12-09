6a536821cbac40d89f3e24ca65f89a8b98ab1a62
yoshira abrros <barrosyoshira@gmail.com>
2025-12-09 14:21:56 -0500

    Initialize project

---
+++ src/test/java/com/codeup/CoopCredit/CoopCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.CoopCredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
